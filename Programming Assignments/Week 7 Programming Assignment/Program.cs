ccc1c95ac6018348094e166121bc7363dd0b11b3
Paul <paulsamuel77@gmail.com>
2021-05-03 18:51:58 +0800

    Week 7 Heap Data structure MinHeap Done

--- Programming Assignments/Week 7 Programming Assignment/Program.cs
+++ Programming Assignments/Week 7 Programming Assignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Week_7_Programming_Assignment
 {
@@ -10,4 +12,134 @@ namespace Week_7_Programming_Assignment
             Console.ReadKey();
         }
     }
+
+    class FileProcessor
+    {
+        public int[] ReadTextFile(string directory)
+        {
+            string[] lines = File.ReadAllLines(directory);
+            int[] intArray = Array.ConvertAll(lines, s => Convert.ToInt32(s));
+
+            return intArray;
+        }
+    }
+
+    class MedianMaintenance
+    {
+        
+
+        public int FindMedian(int number)
+        {
+            return 0;
+        }
+    }
+
+    public class MinIntHeap
+    {
+        int _capacity;
+        int _size = 0; 
+        int[] _items;
+
+        public MinIntHeap(int capacity = 10)
+        {
+            _capacity = capacity;
+            _items = new int[_capacity];
+        }
+
+        int getLeftChildIndex(int parentIndex) {return 2 * parentIndex + 1;}
+        int getRightChildIndex(int parentIndex) {return 2 * parentIndex + 2;}
+        int getParentIndex(int childIndex) {return (childIndex - 1)/2;}
+
+        bool hasLeftChild(int index) {return getLeftChildIndex(index) < _size ;}
+        bool hasRightChild(int index) {return getRightChildIndex(index) < _size;}
+        bool hasParent(int index) {return getParentIndex(index) >= 0;}
+        
+        int leftChild(int index) {return _items[getLeftChildIndex(index)];}
+        int rightChild(int index) {return _items[getRightChildIndex(index)];}
+        int parent(int index) {return _items[getParentIndex(index)];}
+
+        void Swap(int indexOne, int indexTwo)
+        {
+            int temp = _items[indexOne];
+            _items[indexOne] = _items[indexTwo];
+            _items[indexTwo] = temp;
+        }
+
+        void EnsureExtraCapacity()
+        {
+            if(_size == _capacity)
+            {
+                Array.Resize(ref _items, _capacity*2);
+                _capacity *= 2;
+            }
+        }
+
+        public int peek()
+        {
+            if (_size == 0) 
+            {
+                throw new ArgumentOutOfRangeException("Tree size is zero so there is nothing to see");
+            }
+            return _items[0];
+        }
+
+        public int pull()
+        {
+            if (_size == 0)
+            {
+                throw new ArgumentOutOfRangeException("Tree size is zero so we can no longer pull");
+            }
+            int item = _items[0];
+            _items[0] = _items[_size-1];
+            _size--;
+            HeapifyDown();    
+            return item;
+        }
+
+        public void add(int item)
+        {
+            EnsureExtraCapacity();
+            _items[_size] = item;
+            _size++;
+            HeapifyUp();
+        }
+
+        public void HeapifyUp()
+        {
+            int index = _size -1;
+            while(hasParent(index) && parent(index) > _items[index])
+            {
+                Swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }
+        }
+
+        public void HeapifyDown()
+        {
+            int index = 0;
+            while(hasLeftChild(index))
+            {
+                int smallerChildIndex = getLeftChildIndex(index);
+                if (hasRightChild(index) && rightChild(index) < leftChild(index))
+                {
+                    smallerChildIndex = getRightChildIndex(index);    
+                }
+                if (_items[index] < _items[smallerChildIndex])
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }
+        }
+
+        public int GetHeapSize()
+        {
+            return _items.Length;
+        }
+    }
+
 }
--- Programming Assignments/Week 7 Programming Assignment/testclass.cs
+++ Programming Assignments/Week 7 Programming Assignment/testclass.cs
@@ -1,10 +1,131 @@
 using Xunit;
+using Week_7_Programming_Assignment;
+using System;
 
 public class testclass
 {
     [Fact]
-    public void TestTheTestClass()
+    public void MinHashTest()
     {
-        Assert.Equal(2,2);
+        // Test for MinHash Creation, add, and pull
+        // Arrange
+        MinIntHeap minIntHeap = new MinIntHeap();
+        int[] arrayAnswer = {10, 15, 20, 17};
+        int[] myArray = new int[4];
+
+        // Act
+        minIntHeap.add(10);
+        minIntHeap.add(15);
+        minIntHeap.add(20);
+        minIntHeap.add(17);
+
+        // Assert
+        Assert.Equal(10, minIntHeap.pull());
+        Assert.Equal(15, minIntHeap.pull());
+        Assert.Equal(17, minIntHeap.pull());
+        Assert.Equal(20, minIntHeap.pull());
+    }
+
+    [Fact]
+    public void MinHashNodePullFuncTests()
+    {
+        // Arrange
+        MinIntHeap minIntHeap = new MinIntHeap();
+        // 10 15 20 17
+        int[] arrayAnswer = {15, 17, 20};
+        int[] myArray = new int[4];
+        minIntHeap.add(10);
+        minIntHeap.add(15);
+        minIntHeap.add(20);
+        minIntHeap.add(17);
+
+        // Act
+        minIntHeap.pull();
+        
+        // Assert
+        Assert.Equal(15, minIntHeap.pull());
+    }
+
+    public void MinHashNodePushFuncTests()
+    {
+        // Arrange
+        MinIntHeap minIntHeap = new MinIntHeap();
+        // 15, 17, 20
+        int[] arrayAnswer = {5, 15, 17, 20};
+        int[] myArray = new int[4];
+
+        // Act
+        minIntHeap.add(10);
+        minIntHeap.add(15);
+        minIntHeap.add(20);
+        minIntHeap.add(17);
+        minIntHeap.pull();
+        minIntHeap.add(5);
+
+        // Assert
+        Assert.Equal(5, minIntHeap.pull());
+        Assert.Equal(15, minIntHeap.pull());
+        Assert.Equal(17, minIntHeap.pull());
+        Assert.Equal(20, minIntHeap.pull());
+    }
+
+    [Fact]
+    public void MinIntHashOverflowTest()
+    {
+        // Arrange
+        MinIntHeap minIntHeap = new MinIntHeap(4);
+        int[] arrayAnswer = {5, 10, 15, 20, 17};
+        int[] myArray = new int[5];
+
+        // Act
+        minIntHeap.add(10);
+        minIntHeap.add(15);
+        minIntHeap.add(20);
+        minIntHeap.add(17);
+        minIntHeap.add(5);
+        
+        // Assert
+        Assert.Equal(8,minIntHeap.GetHeapSize()); 
+    }
+
+    [Fact]
+    public void Pull_ZeroSizeTree_ThrowsArgumentOutofRangeException()
+    {
+        // Arrange
+        MinIntHeap minIntHeap = new MinIntHeap(0);
+
+        // Assert
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => minIntHeap.pull());
+        Assert.Contains("Tree size is zero", ex.Message);
+    }
+
+    public void Peek_ZeroSizeTree_ThrowsArgumentOutOfRangeException()
+    {
+        MinIntHeap minIntHeap = new MinIntHeap(0);
+
+        // Assert
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => minIntHeap.peek());
+        Assert.Contains("Tree size is zero", ex.Message);
+    }
+
+    [Fact]
+    public void FileReaderClassTest()
+    {   
+    // Arrange
+        FileProcessor fileProcessor = new FileProcessor();
+        int[] testAnswer = {1, 666, 10, 667, 100, 2 ,3};
+    
+    // Act
+        int[] myAnswer = fileProcessor.ReadTextFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 7 Programming Assignment\TestCases\TestCase1.txt");
+
+    // Assert
+        // Test of size
+        Assert.Equal(testAnswer.Length, myAnswer.Length);
+
+        // Test of values
+        for(int i = 0; i < testAnswer.Length; i++)
+        {
+            Assert.Equal(testAnswer[i], myAnswer[i]);
+        }
     }
 }
