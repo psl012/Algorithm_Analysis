480a9c79336960a71792a1a1544662895579a210
Paul <paulsamuel77@gmail.com>
2021-05-03 11:43:15 +0800

    Initial Commit for Week 7 Programming Assignment

---
+++ Programming Assignments/Week 7 Programming Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week_7_Programming_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming Assignments/Week 7 Programming Assignment/testclass.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+public class testclass
+{
+    [Fact]
+    public void TestTheTestClass()
+    {
+        Assert.Equal(2,2);
+    }
+}
