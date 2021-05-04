6752a33725d622b2445f300e35654ff1437dc0c3
Paul <paulsamuel77@gmail.com>
2021-05-04 14:10:11 +0800

    Week 7 Test Cases are passing!

--- Programming Assignments/Week 7 Programming Assignment/Program.cs
+++ Programming Assignments/Week 7 Programming Assignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 
 namespace Week_7_Programming_Assignment
@@ -12,7 +13,6 @@ namespace Week_7_Programming_Assignment
             Console.ReadKey();
         }
     }
-
     class FileProcessor
     {
         public int[] ReadTextFile(string directory)
@@ -26,18 +26,93 @@ namespace Week_7_Programming_Assignment
 
     class MedianMaintenance
     {
-        
+        public List<int> _listOfMedian {get; private set;} = new List<int>();
+        MaxIntHeap _h1 = new MaxIntHeap();
+        MinIntHeap _h2 = new MinIntHeap();
+        int _round = 0;
 
         public int FindMedian(int number)
         {
-            return 0;
+            int medianCandidate;
+            if(_listOfMedian.Count == 0)
+            {
+                _h1.add(number);
+                medianCandidate = number;
+            }
+            else if(_listOfMedian.Count == 1)
+            {
+                if(number < _h1.peek())
+                {
+                    _h1.add(number);
+                }
+                else
+                {
+                    _h2.add(number);
+                }
+                medianCandidate = _h1.peek();
+            }
+
+            else
+            {
+                if(number < _h1.peek())
+                {
+                    _h1.add(number);
+ 
+                }
+                else if (number > _h2.peek())
+                {
+                    _h2.add(number);
+     
+                }
+                else
+                {
+                    _h1.add(number);
+                }
+
+                if(_h1._size - _h2._size == 0)
+                {
+                    medianCandidate = _h1.peek();
+                }
+                else if(_h1._size - _h2._size == 1)
+                {
+                    medianCandidate = _h1.peek();
+                }
+                else if(_h2._size - _h1._size ==1)
+                {
+                    medianCandidate = _h2.peek();
+                }
+                else
+                {
+                    medianCandidate = _h1.peek();
+                }
+
+            }
+
+            if(_h1._size - _h2._size >= 2)
+            {
+                _h2.add(_h1.pull());
+                medianCandidate = _h1.peek();
+            }
+            else if(_h2._size - _h1._size >= 2)
+            {
+                _h1.add(_h2.pull());
+                medianCandidate = _h1.peek();
+            } 
+
+            _listOfMedian.Add(medianCandidate);
+            return medianCandidate;
+        }
+
+        public int GetFinalAnswer()
+        {
+            return _listOfMedian.Sum() % 10000;
         }
     }
 
     public class MinIntHeap
     {
         int _capacity;
-        int _size = 0; 
+        public int _size {get; private set;} = 0; 
         int[] _items;
 
         public MinIntHeap(int capacity = 10)
@@ -141,5 +216,114 @@ namespace Week_7_Programming_Assignment
             return _items.Length;
         }
     }
+
+    public class MaxIntHeap
+    {
+        int _capacity;
+        public int _size {get; private set;}  = 0; 
+        int[] _items;
+
+        public MaxIntHeap(int capacity = 10)
+        {
+            _capacity = capacity;
+            _items = new int[_capacity];
+        }
+
+        int getLeftChildIndex(int parentIndex) {return 2 * parentIndex + 1;}
+        int getRightChildIndex(int parentIndex) {return 2 * parentIndex + 2;}
+        int getParentIndex(int childIndex) {return (childIndex - 1)/2;}
+
+        bool hasLeftChild(int index) {return getLeftChildIndex(index) < _size ;}
+        bool hasRightChild(int index) {return getRightChildIndex(index) < _size;}
+        bool hasParent(int index) {return getParentIndex(index) >= 0;}
+        
+        int leftChild(int index) {return _items[getLeftChildIndex(index)];}
+        int rightChild(int index) {return _items[getRightChildIndex(index)];}
+        int parent(int index) {return _items[getParentIndex(index)];}
+
+        void Swap(int indexOne, int indexTwo)
+        {
+            int temp = _items[indexOne];
+            _items[indexOne] = _items[indexTwo];
+            _items[indexTwo] = temp;
+        }
+
+        void EnsureExtraCapacity()
+        {
+            if(_size == _capacity)
+            {
+                Array.Resize(ref _items, _capacity*2);
+                _capacity *= 2;
+            }
+        }
+
+        public int peek()
+        {
+            if (_size == 0) 
+            {
+                throw new ArgumentOutOfRangeException("Tree size is zero so there is nothing to see");
+            }
+            return _items[0];
+        }
+
+        public int pull()
+        {
+            if (_size == 0)
+            {
+                throw new ArgumentOutOfRangeException("Tree size is zero so we can no longer pull");
+            }
+            int item = _items[0];
+            _items[0] = _items[_size-1];
+            _size--;
+            HeapifyDown();    
+            return item;
+        }
+
+        public void add(int item)
+        {
+            EnsureExtraCapacity();
+            _items[_size] = item;
+            _size++;
+            HeapifyUp();
+        }
+
+        public void HeapifyUp()
+        {
+            int index = _size -1;
+            while(hasParent(index) && parent(index) < _items[index])
+            {
+                Swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }
+        }
+
+        public void HeapifyDown()
+        {
+            int index = 0;
+            while(hasLeftChild(index))
+            {
+                int smallerChildIndex = getLeftChildIndex(index);
+                if (hasRightChild(index) && rightChild(index) > leftChild(index))
+                {
+                    smallerChildIndex = getRightChildIndex(index);    
+                }
+                if (_items[index] > _items[smallerChildIndex])
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }
+        }
+
+        public int GetHeapSize()
+        {
+            return _items.Length;
+        }
+    }
+
 
 }
--- Programming Assignments/Week 7 Programming Assignment/testclass.cs
+++ Programming Assignments/Week 7 Programming Assignment/testclass.cs
@@ -46,6 +46,7 @@ public class testclass
         Assert.Equal(15, minIntHeap.pull());
     }
 
+    [Fact]
     public void MinHashNodePushFuncTests()
     {
         // Arrange
@@ -89,23 +90,116 @@ public class testclass
     }
 
     [Fact]
+    public void MaxIntHeap_Test()
+    {
+                // Test for MinHash Creation, add, and pull
+        // Arrange
+        MaxIntHeap maxIntHeap = new MaxIntHeap();
+        int[] arrayAnswer = {10, 15, 20, 17};
+        int[] myArray = new int[4];
+
+        // Act
+        maxIntHeap.add(10);
+        maxIntHeap.add(15);
+        maxIntHeap.add(20);
+        maxIntHeap.add(17);
+
+        // Assert
+        Assert.Equal(20, maxIntHeap.pull());
+        Assert.Equal(17, maxIntHeap.pull());
+        Assert.Equal(15, maxIntHeap.pull());
+        Assert.Equal(10, maxIntHeap.pull());
+    }
+
+    [Fact]
+    public void MaxHashNodePullFuncTests()
+    {
+        // Arrange
+        MaxIntHeap maxIntHeap = new MaxIntHeap();
+        // 10 15 20 17
+        int[] arrayAnswer = {15, 17, 20};
+        int[] myArray = new int[4];
+        maxIntHeap.add(10);
+        maxIntHeap.add(15);
+        maxIntHeap.add(20);
+        maxIntHeap.add(17);
+
+        // Act
+        maxIntHeap.pull();
+        
+        // Assert
+        Assert.Equal(17, maxIntHeap.pull());
+    }
+
+    [Fact]
+    public void MaxHashNodePushFuncTests()
+    {
+        // Arrange
+        MaxIntHeap maxIntHeap = new MaxIntHeap();
+        // 15, 17, 20
+        int[] arrayAnswer = {5, 15, 17, 20};
+        int[] myArray = new int[4];
+
+        // Act
+        maxIntHeap.add(10);
+        maxIntHeap.add(15);
+        maxIntHeap.add(20);
+        maxIntHeap.add(17);
+        maxIntHeap.pull();
+        maxIntHeap.add(5);
+
+        // Assert
+        Assert.Equal(17, maxIntHeap.pull());
+        Assert.Equal(15, maxIntHeap.pull());
+        Assert.Equal(10, maxIntHeap.pull());
+        Assert.Equal(5, maxIntHeap.pull());
+    }
+
+    [Fact]
+    public void MaxIntHashOverflowTest()
+    {
+        // Arrange
+        MaxIntHeap maxIntHeap = new MaxIntHeap(4);
+        int[] arrayAnswer = {5, 10, 15, 20, 17};
+        int[] myArray = new int[5];
+
+        // Act
+        maxIntHeap.add(10);
+        maxIntHeap.add(15);
+        maxIntHeap.add(20);
+        maxIntHeap.add(17);
+        maxIntHeap.add(5);
+        
+        // Assert
+        Assert.Equal(8,maxIntHeap.GetHeapSize()); 
+    }
+
+    [Fact]
     public void Pull_ZeroSizeTree_ThrowsArgumentOutofRangeException()
     {
         // Arrange
         MinIntHeap minIntHeap = new MinIntHeap(0);
+        MaxIntHeap maxIntHeap = new MaxIntHeap(0);
 
         // Assert
         var ex = Assert.Throws<ArgumentOutOfRangeException>(() => minIntHeap.pull());
         Assert.Contains("Tree size is zero", ex.Message);
+
+        var ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => maxIntHeap.pull());
+        Assert.Contains("Tree size is zero", ex2.Message);
     }
 
     public void Peek_ZeroSizeTree_ThrowsArgumentOutOfRangeException()
     {
         MinIntHeap minIntHeap = new MinIntHeap(0);
+        MaxIntHeap maxIntHeap = new MaxIntHeap(0);
 
         // Assert
         var ex = Assert.Throws<ArgumentOutOfRangeException>(() => minIntHeap.peek());
         Assert.Contains("Tree size is zero", ex.Message);
+    
+        var ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => maxIntHeap.peek());
+        Assert.Contains("Tree size is zero", ex2.Message);
     }
 
     [Fact]
@@ -127,5 +221,88 @@ public class testclass
         {
             Assert.Equal(testAnswer[i], myAnswer[i]);
         }
+    }
+
+    [Fact]
+    public void TestCase1_FindMedianTest()
+    {
+        // Arrange
+        FileProcessor fileProcessor = new FileProcessor();
+        MedianMaintenance medianMaintenance = new MedianMaintenance();
+        int[] testData = fileProcessor.ReadTextFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 7 Programming Assignment\TestCases\TestCase1.txt");
+        int[] correctAnswer = {1,1,10,10,100,10,10};
+               
+        // Act
+        foreach(int num in testData)
+        {
+            medianMaintenance.FindMedian(num);
+        }
+
+        // Assert
+        for(int i=0; i < correctAnswer.Length; i++)
+        {
+            Assert.Equal(correctAnswer[i], medianMaintenance._listOfMedian[i]);
+        }
+        
+    }
+
+    [Fact]
+    public void TestCase1_FinalAnswerTest()
+    {
+        // Arrange
+        FileProcessor fileProcessor = new FileProcessor();
+        MedianMaintenance medianMaintenance = new MedianMaintenance();
+        int[] testData = fileProcessor.ReadTextFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 7 Programming Assignment\TestCases\TestCase1.txt");
+
+        // Act
+        foreach(int num in testData)
+        {
+            medianMaintenance.FindMedian(num);
+        }
+
+        // Assert
+        Assert.Equal(142, medianMaintenance.GetFinalAnswer());
+    }
+
+    [Fact]
+    public void TestCase2_FindMedianTest()
+    {
+        // Arrange
+        FileProcessor fileProcessor = new FileProcessor();
+        MedianMaintenance medianMaintenance = new MedianMaintenance();
+        int[] testData = fileProcessor.ReadTextFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 7 Programming Assignment\TestCases\TestCase2.txt");
+        int[] correctAnswer = {6331, 2793, 2793, 2793, 2793, 1640, 2793, 2303, 2793, 2303};
+          
+
+        // Act
+        foreach(int num in testData)
+        {
+            medianMaintenance.FindMedian(num);
+        }
+
+        // Assert
+        for(int i=0; i < correctAnswer.Length; i++)
+        {
+            Assert.Equal(correctAnswer[i], medianMaintenance._listOfMedian[i]);
+        }
+        
+    }
+
+    [Fact]
+    public void TestCase2_FinalAnswerTest()
+    {
+        // Arrange
+        FileProcessor fileProcessor = new FileProcessor();
+        MedianMaintenance medianMaintenance = new MedianMaintenance();
+        int[] testData = fileProcessor.ReadTextFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 7 Programming Assignment\TestCases\TestCase2.txt");
+
+        // Act
+        foreach(int num in testData)
+        {
+            medianMaintenance.FindMedian(num);
+        }
+
+        // Assert
+        Assert.Equal(9335, medianMaintenance.GetFinalAnswer());
     }
 }
