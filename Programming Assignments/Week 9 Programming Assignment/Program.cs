0a5bdb8b332057a0d115b1099a2a948d28f3dbee
Paul <paulsamuel77@gmail.com>
2021-05-07 17:41:18 +0800

    Initial commmit for Programming Exercise 9

---
+++ Programming Assignments/Week 9 Programming Assignment/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Week_9_Programming_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+         /**   FileReader fileReader = new FileReader();
+            (int, Job[]) myJobData = fileReader.ReadJobFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 9 Programming Assignment\TestCases\Test Case 1.txt");
+            JobScheduler jobScheduler = new JobScheduler();
+            Job[] gg = jobScheduler.DifferenceCriterion(myJobData.Item2);
+
+            foreach(Job j in gg)
+            {
+                Console.WriteLine(j._jobValue);
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+
+    class JobScheduler
+    {
+        QuickSort _quickSort = new QuickSort();
+        public Job[] DifferenceCriterion(Job[] arrayOfJobs)
+        {
+            ClearJobValues(arrayOfJobs);
+            for(int i=0; i<arrayOfJobs.Length; i++)
+            {
+                arrayOfJobs[i]._jobValue = arrayOfJobs[i]._weight - arrayOfJobs[i]._length;
+            }
+            _quickSort.Sort(arrayOfJobs, 0, arrayOfJobs.Length - 1);
+            
+            return arrayOfJobs;
+        }
+
+        void ClearJobValues(Job[] arrayOfJobs)
+        {
+            foreach(Job j in arrayOfJobs)
+            {
+                j._jobValue = -9999;
+            }
+        }
+    }
+
+    class FileReader
+    {
+        public (int,Job[]) ReadJobFile(string dir)
+        {
+            string[] line = File.ReadAllLines(dir);
+            int numberOfJobs = Convert.ToInt32(line[0]);
+            Job[] arrayOfJobs = new Job[numberOfJobs];
+            
+            for(int i=1; i< line.Length; i++)
+            {
+                string[] temp = line[i].Split();
+                int weight = Convert.ToInt32(temp[0]);
+                int length = Convert.ToInt32(temp[1]);
+                arrayOfJobs[i - 1] = new Job(weight, length);
+            }
+
+            return (numberOfJobs, arrayOfJobs);
+        }
+    }
+
+    class Job
+    {
+        public int _weight {get; private set;}
+        public int _length {get; private set;}
+        public int _jobValue;
+
+        public Job(int weight, int length, int jobValue = -9999)
+        {
+            _weight = weight;
+            _length = length;
+            _jobValue = jobValue;
+        }
+    }
+
+    class QuickSort
+    {
+        int Partition(Job[] array, int low,
+                                        int high)
+        {
+            //1. Select a pivot point.
+            int pivot = array[high]._jobValue;
+
+            int lowIndex = (low - 1);
+
+            //2. Reorder the collection.
+            for (int j = low; j < high; j++)
+            {
+                if (array[j]._jobValue >= pivot)
+                {
+                    lowIndex++;
+
+                    Job temp = array[lowIndex];
+                    array[lowIndex] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            Job temp1 = array[lowIndex + 1];
+            array[lowIndex + 1] = array[high];
+            array[high] = temp1;
+
+            return lowIndex + 1;
+        }
+
+        public void Sort(Job[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitionIndex = Partition(array, low, high);
+
+                //3. Recursively continue sorting the array
+                Sort(array, low, partitionIndex - 1);
+                Sort(array, partitionIndex + 1, high);
+            }
+        }
+    }
+}
---
+++ Programming Assignments/Week 9 Programming Assignment/TestClass.cs
@@ -0,0 +1,72 @@
+using Xunit;
+using Week_9_Programming_Assignment;
+
+public class TestClass
+{
+    [Fact]
+    public void JobFileReadTest()
+    {
+        // Arrange
+        FileReader fileReader = new FileReader();
+        int answerNumberOfJobs = 12;
+        Job[] answerArrayOfJobs = new Job[answerNumberOfJobs];
+        answerArrayOfJobs[0] = new Job(8, 50);
+        answerArrayOfJobs[1] = new Job(74, 59);
+        answerArrayOfJobs[2] = new Job(31, 73);
+        answerArrayOfJobs[3] = new Job(45, 79);
+        answerArrayOfJobs[4] = new Job(24, 10);
+        answerArrayOfJobs[5] = new Job(41, 66);
+        answerArrayOfJobs[6] = new Job(93, 43);
+        answerArrayOfJobs[7] = new Job(88, 4);
+        answerArrayOfJobs[8] = new Job(28, 30);
+        answerArrayOfJobs[9] = new Job(41, 13);
+        answerArrayOfJobs[10] = new Job(4, 70);
+        answerArrayOfJobs[11] = new Job(10, 58);
+
+        (int, Job[]) myJobData;
+
+        // Act
+        myJobData = fileReader.ReadJobFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 9 Programming Assignment\TestCases\Test Case 1.txt");
+
+        // Assert
+        Assert.Equal(answerNumberOfJobs, myJobData.Item1);
+
+        for(int i=0; i<answerArrayOfJobs.Length; i++)
+        {
+            Assert.Equal(answerArrayOfJobs[i]._weight, myJobData.Item2[i]._weight);
+            Assert.Equal(answerArrayOfJobs[i]._length, myJobData.Item2[i]._length);
+        }
+    }
+
+    [Fact]
+    public void DifferenceCriterion_Test()
+    {
+        // Arrange
+        Job[] valueAnswer = new Job[12];
+        valueAnswer[0] = new Job(88, 4, 84);
+        valueAnswer[1] = new Job(93, 43, 50);
+        valueAnswer[2] = new Job(41, 13, 28);   
+        valueAnswer[3] = new Job(74, 59, 15);
+        valueAnswer[4] = new Job(24, 10, 14);
+        valueAnswer[5] = new Job(28, 30, -2);
+        valueAnswer[6] = new Job(41, 66, -25);
+        valueAnswer[7] = new Job(45, 79, -34);
+        valueAnswer[8] = new Job(31, 73, -42);                
+        valueAnswer[9] = new Job(8, 50, -42);
+        valueAnswer[10] = new Job(10, 58, -48);
+        valueAnswer[11] = new Job(4, 70, -66);
+        
+        FileReader fileReader = new FileReader();
+        (int, Job[]) myJobData = fileReader.ReadJobFile(@"C:\Users\Paul\Documents\Open Source Society for Computer Science (OSSU)\Algorithms Coursera\Programming Assignments\Week 9 Programming Assignment\TestCases\Test Case 1.txt");
+        JobScheduler jobScheduler = new JobScheduler();
+        
+        // Act
+        Job[] myJobAnswer = jobScheduler.DifferenceCriterion(myJobData.Item2);
+
+        // Assert
+        for(int i=0; i < 12; i++)
+        {
+            Assert.Equal(valueAnswer[i]._jobValue, myJobAnswer[i]._jobValue);
+        }
+    }
+}
